30d1027cad030c2a89eb045870b628a36d20f6e4
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2020-12-05 00:59:19 +0100

    Header Styling + Overall element size organisation

--- src/components/recipe-list/recipe-list.component.jsx
+++ src/components/recipe-list/recipe-list.component.jsx
@@ -5,15 +5,16 @@ import RecipeItem from "../recipe-item/recipe-item.component";
 import './recipe-list.styles.scss';
 
 const RecipeList = (props) => (
-    <div className='recipe-list'>
-        <h1 className='title'>RECIPES</h1>
-        <div className='preview'>
-            {
-                props.recipes
-                    .map(({id, ...otherRecipeProps}) => (
-                    <RecipeItem key={id} {...otherRecipeProps}/>
-                ))
-            }
+    <div>
+        <div className='recipe-list'>
+            <div className='preview'>
+                {
+                    props.recipes
+                        .map(({id, ...otherRecipeProps}) => (
+                            <RecipeItem key={id} {...otherRecipeProps}/>
+                        ))
+                }
+            </div>
         </div>
     </div>
 )
