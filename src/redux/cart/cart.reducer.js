c93ebb2675fb3bb2256328fc1ccef0854f43bb3c
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2020-12-15 01:25:47 +0100

    Quantity adjustment (add item / remove item) on arrow clicks

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { addItemToCart } from "./cart.utils";
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
@@ -22,6 +22,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
         default:
             return state;
